d27d3f528a669a9a631ed44b05a16969b7efe6a6
Tilda Källström <tildakallstrom@gmail.com>
2023-04-26 17:16:57 +0200

    Added exercise 2

---
+++ exercise2/js.js
@@ -0,0 +1,19 @@
+const tempen = {
+    "stad" : [
+    {"namn" : "Malmö", "natt" : "12", "morgon" : "14","kvall" : "10"},
+    {"namn" : "Göteborg", "natt" : "11", "morgon" : "15","kvall" : "9"},
+    {"namn" : "Stockholm", "natt" : "10", "morgon" : "13","kvall" : "10"},
+    {"namn" : "Mariestad", "natt" : "11", "morgon" : "16","kvall" : "11"}
+    ]
+    };
+
+    //loop through data
+    for (let i = 0; i < tempen.stad.length; i++) {
+        let stad = tempen.stad[i];
+
+        //if stad = Stockholm, write out data
+        if (stad.namn === "Stockholm") {
+            let data = `Data for Stockholm:\nNatt: ${stad.natt}\nMorgon: ${stad.morgon}\nKväll: ${stad.kvall}`;
+            alert(data);
+        }
+    }
